86de918c5064a983f4522722f4559278254eb18e
jinna239 <jinna239@gmail.com>
2022-09-06 23:25:28 +0530

    Vetri's code

--- src/test/java/org/test/SampleJava.java
+++ src/test/java/org/test/SampleJava.java
@@ -3,6 +3,9 @@ package org.test;
 public class SampleJava {
 
 	public static void main(String[] args) {
+		System.out.println("Test4");
+		System.out.println("Test5");
+		System.out.println("Test6");
 		
 	}
 }
